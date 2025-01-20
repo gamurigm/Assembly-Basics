320f8c1229e97cf4ad4cc9cda38e4888271eb3f8
gabriel murillo <gamurigm@gmail.com>
2025-01-20 08:30:48 -0500

    T2.2

--- Taller2.2_Murillo.asm
+++ Taller2.2_Murillo.asm
@@ -1,41 +1,32 @@
 org 100h
-
 inicio:        
     mov M, 9
-          
-ciclo2:
-    mov N, 9       
-    
-    mov ah, 2      
-    mov dl, M      
-    add dl, 48   
-    int 21h
-    
-ciclo1:
-    ; Muestra N
+    mov N, 9
+
+ciclo:
     mov ah, 2
-    mov dl, N      ; Obtiene valor de N
-    add dl, 30h    ; Convierte a ASCII
+    mov dl, M
+    add dl, 48    ; Convierte a ASCII
     int 21h
     
-    ; Espacio entre números
-    mov dl, 20h    ; Espacio en ASCII
+    mov ah, 2
+    mov dl, N
+    add dl, 48
     int 21h
-    
-    dec N          ; Decrementa N
-    jnz ciclo1
-    
-    ; Nueva línea
+     
     mov ah, 2
     mov dl, 0Dh    ; Retorno de carro
     int 21h
-    mov dl, 0Ah    ; Nueva línea
+    mov dl, 0Ah    ; Nueva linea
     int 21h
     
-    dec M          ; Decrementa M
-    jnz ciclo2
-
+    dec N          
+    jge ciclo      ; Continúa si N >= 0
+    
+    mov N, 9       ; Reinicia N
+    dec M         
+    jge ciclo      ; Continua si M >= 0
+    
 ret        
-
 M db 0
 N db 0
